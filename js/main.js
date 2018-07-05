1cb9b37ea415904966cd8a510111b366904262a5
gfed53 <gfederico7153@gmail.com>
2018-07-05 17:07:53 -0400

    Updated text animation.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ $(() => {
   const $h1 = $('#text--main');
   $h1.html(breakIntoSpans($h1));
 
+  setAnimationDelay($h1);
+
 });
 
 // Grab text of passed $el, and return text where each letter is in its own <span>
@@ -21,5 +23,18 @@ function breakIntoSpans($el){
 
   // console.log('result',result);
 
+}
 
+// Attach an 'animation-delay' property/value to each child in $el, where each child has an incremented delay value
+
+function setAnimationDelay($el) {
+  let delayVal = 0;
+  const $children = $el.children('span');
+  // console.log('children',children);
+
+  $children.each((i,child) => {
+    console.log('child',child);
+    $(child).css('animation-delay', `${delayVal}ms`);
+    delayVal += 100;
+  });
 }
